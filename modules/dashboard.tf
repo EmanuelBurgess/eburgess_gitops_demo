2b1a8645b2a9bf700038acb61ef5b10bc5ac6f38
Emanuel-Burgess <emanuel@mavenslist.app>
2021-04-26 23:52:56 -0400

    testing

---
+++ modules/dashboard.tf
@@ -0,0 +1,40 @@
+resource "google_monitoring_alert_policy" "alert_policy" {
+  display_name = "Manny Website demo dashboard"
+  combiner     = "OR"
+  conditions {
+    display_name = "montior apache webserver"
+    condition_threshold {
+      filter          = "metric.type=\"compute.googleapis.com/instance/instance/cpu/utilization\" AND resource.type=\"gce_instance\""
+      duration        = "60s"
+      comparison      = "COMPARISON_GT"
+      threshold_value = 1
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_RATE"
+      }
+    }
+  }
+
+  user_labels = {
+    name = "webserver demo"
+  }
+}
+
+resource "google_monitoring_notification_channel" "slack" {
+  display_name = "Test Slack Channel"
+  type         = "slack"
+  labels = {
+    "channel_name" = "#tf-gitops-channel"
+  }
+  sensitive_labels {
+    auth_token = "present.txt"
+  }
+}
+
+resource "google_monitoring_notification_channel" "email" {
+  display_name = "Test Notification Channel"
+  type         = "email"
+  labels = {
+    email_address = "emanuelburgess@gmail.com"
+  }
+}
