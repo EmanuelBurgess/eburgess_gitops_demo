5270b7fe3beccda6b86303dadeffd9dd1f3bd412
Emanuel-Burgess <emanuel@mavenslist.app>
2021-04-27 00:58:38 -0400

    udpate

--- modules/monitoringandalerting.tf
+++ modules/monitoringandalerting.tf
@@ -4,7 +4,7 @@ resource "google_monitoring_alert_policy" "alert_policy" {
   conditions {
     display_name = "montior apache webserver"
     condition_threshold {
-      filter          = "metric.type=\"compute.googleapis.com/instance/instance/cpu/utilization\" AND resource.type=\"gce_instance\""
+      filter          = "metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.type=\"gce_instance\""
       duration        = "60s"
       comparison      = "COMPARISON_GT"
       threshold_value = 1
